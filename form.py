d50d05f90bb20637bceff4f41a0f194f105f2597
lzm <944581651@qq.com>
2018-12-04 14:52:56 -0500

    change form

--- form.py
+++ form.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, IntegerField
-from wtforms.validators import DataRequired, EqualTo, ValidationError, AnyOf
+from wtforms.validators import DataRequired, EqualTo, ValidationError
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember Me')
+    is_manager = BooleanField('Manager Mode')
     submit = SubmitField('Sign In')
 
 
@@ -15,13 +16,12 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     name = StringField('Name', validators=[DataRequired()])
-    type = StringField('Type (Individual or Business)', validators=[DataRequired(), AnyOf(['Individual', 'Business'])])
-    address = StringField('Address')
+    email = StringField('E-mail')
     phone_number = IntegerField('Phone Number')
     submit = SubmitField('Register')
 
     def validate_username(self, username):
         from app import User
-        user = User().query_by_username(username.data)
+        user = User().query_by_username(username.data, False)
         if user is not None:
             raise ValidationError('Please use a different username.')
