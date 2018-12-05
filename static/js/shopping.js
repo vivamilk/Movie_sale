fa88a77aa8a4c1c9f9eed3f05d9ddb70648ffb4a
Deng Pan <dep90@pitt.edu>
2018-12-05 15:52:51 -0500

    add ajax function on button

--- static/js/shopping.js
+++ static/js/shopping.js
@@ -1,16 +1,40 @@
+function get_all_button() {
+    return $(".movie-list").find("button")
+}
+
 $(document).ready(function () {
-        $("button#3").click(function () {
-            $.ajax({
-                url: '/shopping/3',
-                contentType: "application/json; charset=utf-8",
-                type:"POST",
-                data: JSON.stringify({'number':1}),
-                success: function(response) {
-                console.log(response);
-                },
-                error: function(error) {
-                    console.log(error);
-                }
-            });
+    let but = get_all_button();
+    $('.movie-list').delegate('button', "click", function(event){
+        var movie_id = this.id;
+         $.ajax({
+            url: '/shopping/' + movie_id,
+            contentType: "application/json; charset=utf-8",
+            type:"POST",
+            data: JSON.stringify({'number':1}),
+            success: function(response) {
+            console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
         });
-});
+    });
+
+    //
+    // $("button#3").click(function () {
+    //     $.ajax({
+    //         url: '/shopping/3',
+    //         contentType: "application/json; charset=utf-8",
+    //         type:"POST",
+    //         data: JSON.stringify({'number':1}),
+    //         success: function(response) {
+    //         console.log(response);
+    //         },
+    //         error: function(error) {
+    //             console.log(error);
+    //         }
+    //     });
+    // });
+})
+// span class = badge  press the button will increase the number of badge
+//
